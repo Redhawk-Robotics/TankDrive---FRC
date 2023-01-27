c8c58a0bd8b899cb6553c4c9f348bdbb9987b664
Giankyle Vallarta <kyle101206@users.noreply.github.com>
2023-01-26 21:48:45 -0500

    DONE

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,9 +2,27 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/**
+ * @Filename:Robot.java
+ * @Purpose:This Robot.java File is for the functionality for the entirety of the TankDrive 
+ * @Version:1.0
+ * @Author:RedHawk Robotics FRC 87390`
+ * @Date:1/26/2023
+ */
+
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,23 +35,90 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
-  @Override
-  public void robotInit() {}
+  private final XboxController DRIVER = new XboxController(0); 
+
+  //INIT MOTORS
+  private final static CANSparkMax rightMotor1 = new CANSparkMax(1, MotorType.kBrushless); //
+  private final static CANSparkMax rightMotor2 = new CANSparkMax(2, MotorType.kBrushless);
+  private final static CANSparkMax leftMotor1 = new CANSparkMax(3, MotorType.kBrushless);
+  private final static CANSparkMax leftMotor2 = new CANSparkMax(4, MotorType.kBrushless);
+
+  //GROUP THE MOTORS
+  private final static MotorControllerGroup leftSpeedGroup = new MotorControllerGroup(rightMotor1, rightMotor2);
+  private final static MotorControllerGroup rightSpeedGroup = new MotorControllerGroup(leftMotor1, leftMotor2);
+
+  //CREATING THE DIFFERENTIAL DRIVE OBJECT FOR THE TANKDRIVE
+  protected final static DifferentialDrive robotDrive = new DifferentialDrive(leftSpeedGroup, rightSpeedGroup);
+
+
+  //AUTONS
+  //private final SimpleAuto simpleAuto = new SimpleAuto(startTime, robotDrive);
+  
+  //TO UPLOAD INFORMATION TO SMARTDASHBOARD 
+  SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private String m_autonomousCommand;
 
   @Override
-  public void robotPeriodic() {}
+  public void robotInit() {
+
+    rightMotor1.setIdleMode(IdleMode.kBrake);
+    rightMotor2.setIdleMode(IdleMode.kBrake);
+    leftMotor1.setIdleMode(IdleMode.kBrake);
+    leftMotor2.setIdleMode(IdleMode.kBrake);
+
+    leftSpeedGroup.setInverted(false);
+    rightSpeedGroup.setInverted(true);
+
+    m_chooser.setDefaultOption("Simple Auto", "Simple");
+    ///m_chooser.addOption("Auto ", "Test");
+    SmartDashboard.putData(m_chooser);
+
+  }
 
   @Override
-  public void autonomousInit() {}
+  public void robotPeriodic() {
+    
+    CommandScheduler.getInstance().run();
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousInit() {
+    // startTime = Timer.getFPGATimestamp();
+    m_autonomousCommand = m_chooser.getSelected();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      switch (m_autonomousCommand) {
+        case "simple":
+          SimpleAuto.init();
+          break;
+      }
+    }
+  }
+  
+
+  @Override
+  public void autonomousPeriodic() {
+    if (m_autonomousCommand != null) {
+      switch (m_autonomousCommand) {
+        case "simple":
+          SimpleAuto.periodic();
+          break;
+        default:
+          disabledInit();
+          break;
+      }
+    }
+  }
 
   @Override
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    robotDrive.arcadeDrive(-DRIVER.getLeftY(), DRIVER.getRightX());
+  }
 
   @Override
   public void disabledInit() {}
---
+++ src/main/java/frc/robot/SimpleAuto.java
@@ -0,0 +1,34 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class SimpleAuto extends Robot{
+    private static double startTime;
+
+    /** Creates a new SimpleAuto. */
+    public SimpleAuto(double startTimelocal, DifferentialDrive robotDrive) {
+
+    startTime = startTimelocal;
+  }
+  // Called when the command is initially scheduled.
+  public static void init() {
+    startTime = Timer.getFPGATimestamp();
+  }
+ // Called every time the scheduler runs while the command is scheduled.
+ public static void periodic() {
+    double time = Timer.getFPGATimestamp();
+
+    if (time - startTime < 3) {
+
+    robotDrive.arcadeDrive(0, 0);
+
+    } else if (time - startTime > 3 && time - startTime < 8) {
+   
+    robotDrive.arcadeDrive(.5, 0);
+    
+    } else {
+     robotDrive.arcadeDrive(0, 0);
+    }
+  }
+}
